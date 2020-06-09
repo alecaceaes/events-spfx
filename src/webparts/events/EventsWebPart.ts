0cfb26725bba360569de88b3e56ab1a3581428e4
Alec Lawrence Tecson <tecsonalr@gmail.com>
2020-06-09 22:51:54 +0800

    Crafting and Attaching HTML

--- src/webparts/events/EventsWebPart.ts
+++ src/webparts/events/EventsWebPart.ts
@@ -1,7 +1,8 @@
 import { Version } from '@microsoft/sp-core-library';
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
+  PropertyPaneTextField,
+  PropertyPaneToggle
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { escape } from '@microsoft/sp-lodash-subset';
@@ -11,51 +12,51 @@ import * as strings from 'EventsWebPartStrings';
 
 export interface IEventsWebPartProps {
   description: string;
+  showpastevents: boolean;
 }
+
+import './app/app.module';
+import LandingTemplate from './LandingTemplate';
 
 export default class EventsWebPart extends BaseClientSideWebPart <IEventsWebPartProps> {
 
   public render(): void {
-    this.domElement.innerHTML = `
-      <div class="${ styles.events }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
-          </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+    if (!this.renderedOnce) {
+      // var ajax = new XMLHttpRequest();
+      // ajax.open("GET", "/src/webparts/events/LandingTemplate.html", false);
+      // ajax.send();
+      // this.domElement.innerHTML = ajax.responseText;
+
+      this.domElement.innerHTML = LandingTemplate.templateHtml;
+    }
   }
 
   protected get dataVersion(): Version {
-  return Version.parse('1.0');
-}
+    return Version.parse('1.0');
+  }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-  return {
-    pages: [
-      {
-        header: {
-          description: strings.PropertyPaneDescription
-        },
-        groups: [
-          {
-            groupName: strings.BasicGroupName,
-            groupFields: [
-              PropertyPaneTextField('description', {
-                label: strings.DescriptionFieldLabel
-              })
-            ]
-          }
-        ]
-      }
-    ]
-  };
-}
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                }),
+                PropertyPaneTextField('showpastevents', {
+                  label: strings.ShowPastEventsFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
 }
